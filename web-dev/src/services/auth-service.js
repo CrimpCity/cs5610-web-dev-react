7d23a79f26bcb289d9dfe894213cd7de59543f3d
CrimpCity <tutuianu.georgian@gmail.com>
2022-04-16 17:37:56 -0400

    add first guess at services

---
+++ web-dev/src/services/auth-service.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+const BASE_URL = "http://localhost:4000";
+const AUTH_API = `${BASE_URL}/api/auth`
+
+const api = axios.create({
+    withCredentials: true
+});
+
+export const signup = (user) =>
+    api.post(`${AUTH_API}/signup`, user)
+        .then(response => response.data);
+
+export const profile = () =>
+    api.post(`${AUTH_API}/profile`)
+        .then(response => response.data);
+
+export const logout = (user) =>
+    api.post(`${AUTH_API}/logout`, user)
+        .then(response => response.data);
+
+export const login = (credentials) =>
+    api.post(`${AUTH_API}/login`, credentials)
+        .then(response => response.data);
---
+++ web-dev/src/services/bookmarks-service.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:4000";
+const USERS_API = `${BASE_URL}/api/users`;
+
+
+export const findUserBookmarkedMovie = (uid, mid) =>
+    await axios.get(`${USERS_API}/${uid}/bookmarks/${mid}`)
+        .then(response => response.data);
+
+export const userTogglesBookmark = (uid, mid) =>
+    await axios.get(`${USERS_API}/${uid}/bookmarks/toggle/${mid}`)
+        .then(response => response.data);
+
+export const deleteAllBookmarksOfMovie = mid =>
+    await axios.delete(`${BASE_URL}/api/movie/${mid}/bookmarks`)
+        .then(response => response.data);
+
+export const findAllBookmarkedByUser = uid =>
+    await axios.get(`${BASE_URL}/api/users/${uid}/bookmarks`)
+        .then(response => response.data);
+
+
+
+// const api = axios.create({
+//     withCredentials: true
+// });
+
+// export const findUserBookmarkedMovie = (uid, mid) =>
+//     api.get(`${USERS_API}/${uid}/bookmarks/${mid}`)
+//         .then(response => response.data);
+
+// export const userTogglesBookmark = (uid, mid) =>
+//     api.get(`${USERS_API}/${uid}/bookmarks/toggle/${mid}`)
+//         .then(response => response.data);
+
+// export const deleteAllBookmarksOfMovie = mid =>
+//     api.delete(`${BASE_URL}/api/movie/${mid}/bookmarks`)
+//         .then(response => response.data);
+
+// export const findAllBookmarkedByUser = uid =>
+//     api.get(`${BASE_URL}/api/users/${uid}/bookmarks`)
+//         .then(response => response.data);
