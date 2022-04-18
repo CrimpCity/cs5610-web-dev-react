5896f5ce4b3a790a73e0689b53211d61f82132ba
CrimpCity <tutuianu.georgian@gmail.com>
2022-04-18 17:26:51 -0400

    refactor homepage

--- web-dev/src/components/HomePage/HomePageComponent.js
+++ web-dev/src/components/HomePage/HomePageComponent.js
@@ -2,67 +2,29 @@ import { React, useState, useEffect, useCallback, useMemo } from "react";
 import HomeBannerComponent from "../HomeBanner/HomeBannerComponent.js";
 import NavigationTopBar from "../NavigationTopBar/index.js";
 import BoxArtList from "../BoxArtList/index.js"
+
 import * as moviesService from '../../services/movies-service.js';
+
+
 import './home-page.css';
 
 
+import Trending from "../data/Movie.json";
+import TopPicks from "../data/TopPicks.json";
+
+import TrendingJson from "../data/Movie.json";
+import TopPicksJson from "../data/TopPicks.json";
 
 const HomePageComponent = () => {
-    const [bookmarks, setBookmarks] = useState([]);
-    // useMemo so that the IDs do not get initialized on every render
-    const trendingIDs = useMemo(() => [
-        "625c6a217cdc9b794203a8c9",
-        "625c6a267cdc9b794203a8cb",
-        "625c6a287cdc9b794203a8cd",
-        "625c6a2a7cdc9b794203a8cf",
-        "625c6a2c7cdc9b794203a8d1"], []);
-    const topIDs = useMemo(() => [
-        "625c71477cdc9b794203a8d4",
-        "625c71507cdc9b794203a8d6",
-        "625c71577cdc9b794203a8d8",
-        "625c71627cdc9b794203a8da",
-        "625c71677cdc9b794203a8dc"], []);
-
-    // check if the user has an account
     const loggedInUser = "123";
     // const guest = "guest";
     const guest = "123";
-    let IDs = trendingIDs;
+    let movies = Trending;
     let trendingTitle = "Trending Now";
-    // if the user has an account then show them specific movie suggestions
     if (loggedInUser !== guest) {
-        IDs = topIDs;
+        movies = TopPicks;
         trendingTitle = "Top Picks For " + loggedInUser;
     }
-
-    // Retrieve movies from the database
-    const getMoviesByIDs = async (IDs) => {
-        const promises = await IDs.map(async (mid) => (moviesService.findMovieById(mid)));
-        const resolved = await Promise.all(promises);
-        console.log("resolved");
-        console.log(resolved);
-        setBookmarks(resolved);
-        return resolved;
-    };
-
-    // Need to useCallback so we don't get movies on each rendering
-    const renderMovies = useCallback(
-        () => {
-            const movies = getMoviesByIDs(IDs);
-            return movies;
-        },
-        [IDs]);
-
-
-    // Don't get movies on each rendering
-    useEffect(() => {
-        let isMounted = true;
-        renderMovies();
-        return () => { isMounted = false; }
-    }, [renderMovies]);
-
-    console.log("Movies");
-    console.log(bookmarks);
 
     const formatted = (
         <>
@@ -77,9 +39,15 @@ const HomePageComponent = () => {
 
             <div className="pb-3">
                 <ul className="list-group wd-trending-bg-color">
-                    <li className="list-group-item fw-bold fs-4">{trendingTitle}</li>
+                    <li className="list-group-item fw-bold fs-4">Trending Now (different on ~ every refresh)</li>
                 </ul>
-                <BoxArtList movies={bookmarks} />
+                <BoxArtList movies={TrendingJson} />
+            </div>
+            <div className="pb-3">
+                <ul className="list-group wd-trending-bg-color">
+                    <li className="list-group-item fw-bold fs-4">{loggedInUser}'s List (saved bookmarks)</li>
+                </ul>
+                <BoxArtList movies={TopPicksJson} />
             </div>
 
             <div className="wd-add-height-to-scroll">
