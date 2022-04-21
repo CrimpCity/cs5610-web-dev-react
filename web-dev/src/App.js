8f451964c7c678b02e494990815539fb3e848861
CrimpCity <tutuianu.georgian@gmail.com>
2022-04-20 21:26:48 -0400

    add privacy page

--- web-dev/src/App.js
+++ web-dev/src/App.js
@@ -4,7 +4,7 @@ import './vendors/bootstrap/css/bootstrap.min.css';
 import './vendors/bootstrap/bootstrap.min.css';
 import './vendors/fontawesome/css/all.min.css';
 
-import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import HomePage from "../src/components/HomePage/index.js";
 import Profile from './components/Profile/index.js';
 import SearchScreen from "./components/SearchScreen";
@@ -13,38 +13,40 @@ import LandingPage from "./components/LandingPage";
 import LoginPage from "./components/LoginPage";
 import RegistrationPage from "./components/RegistrationPage";
 import UserComponent from "./components/RegistrationPage/UserComponent";
-
+import PrivacyScreen from "./components/PrivacyPage/index.js"
 // import Netflicks from "./components/index.js";
 
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="container-fluid">
-        <Routes>
-          <Route path="/">
-            <Route index element={(<Navigate to="/landingpage" replace={true}></Navigate>)} />
-            <Route path="homepage"
-                   element={<HomePage />} />
-            <Route path="profile"
-                   element={<Profile />} />
-            <Route index path="search"
-                   element={<SearchScreen />} />
-            <Route path="detail/:movieId"
-                   element={<DetailScreen />} />
-            <Route path="landingpage"
-                   element={<LandingPage />} />
-            <Route path="login"
-                   element={<LoginPage />} />
-            <Route path="registration"
-                   element={<RegistrationPage />} />
-            <Route path="registration/select-avatar"
-                   element={<UserComponent />}/>
-          </Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+       return (
+              <BrowserRouter>
+                     <div className="container-fluid">
+                            <Routes>
+                                   <Route path="/">
+                                          <Route index element={(<Navigate to="/landingpage" replace={true}></Navigate>)} />
+                                          <Route path="homepage"
+                                                 element={<HomePage />} />
+                                          <Route path="profile"
+                                                 element={<Profile />} />
+                                          <Route index path="search"
+                                                 element={<SearchScreen />} />
+                                          <Route index path="privacy"
+                                                 element={<PrivacyScreen />} />
+                                          <Route path="detail/:movieId"
+                                                 element={<DetailScreen />} />
+                                          <Route path="landingpage"
+                                                 element={<LandingPage />} />
+                                          <Route path="login"
+                                                 element={<LoginPage />} />
+                                          <Route path="registration"
+                                                 element={<RegistrationPage />} />
+                                          <Route path="registration/select-avatar"
+                                                 element={<UserComponent />} />
+                                   </Route>
+                            </Routes>
+                     </div>
+              </BrowserRouter>
+       );
 }
 
 
---
+++ web-dev/src/components/PrivacyPage/index.js
@@ -0,0 +1,79 @@
+import React from "react"
+import NavigationTopBar from "../NavigationTopBar/index.js";
+import "./privacy.css"
+
+
+const PrivacyScreen = () => {
+    return (
+        <>
+            <div className="row bg-color">
+                <div >
+                    <NavigationTopBar />
+                </div>
+                <div className="wd-lower-profile wd-pad-siding">
+                    <p className="fw-bold fs-4">Privacy Policy</p>
+                </div>
+                <div className="pt-3 wd-pad-siding">
+                    <p className="fw-bold fs-5 pb-3">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Quisque rutrum sodales eleifend.
+                        Aenean venenatis nunc eget convallis malesuada.
+                        Donec luctus urna sit amet quam venenatis, sit amet feugiat sapien laoreet.
+                        Proin id metus massa. Nam mattis consectetur rutrum.
+                        Proin ut varius nunc.
+                        Proin rhoncus consequat nulla, et gravida est dictum egestas.
+                    </p>
+
+                    <p className="fw-bold fs-5 pb-3">
+                        Maecenas dignissim eros nisl, eu bibendum massa tempor tincidunt.
+                        Sed posuere consectetur egestas.
+                        Sed nec urna at massa congue congue vel sed metus.
+                        Suspendisse dictum faucibus ipsum.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Aenean elementum metus a erat sollicitudin vehicula.
+                        Nam vitae dignissim ipsum.
+                    </p>
+
+                    <p className="fw-bold fs-5 pb-3">
+                        Nam maximus accumsan ex, at cursus justo dictum vel.
+                        Fusce nibh dui, sollicitudin sed bibendum a, mollis quis urna.
+                        Maecenas nec congue nisi, eget consequat ipsum.
+                        Vestibulum vitae dictum dolor.
+                        Duis malesuada eros nec libero convallis ornare.
+                        Pellentesque semper porta sapien, nec laoreet ipsum varius bibendum.
+                        Curabitur urna massa, vestibulum quis nisl non, pellentesque dictum est.
+                        Praesent non dolor a dui rhoncus dignissim eu consectetur lorem.
+                        Fusce condimentum metus felis, nec vestibulum est euismod nec.
+                        Maecenas auctor tincidunt tempor. Cras id vehicula lacus.
+                        In interdum vulputate elit ut condimentum.
+                        Ut dapibus purus semper tempus laoreet.
+                        In quis luctus augue.
+                        Sed at placerat neque, eget finibus libero.
+                    </p>
+
+                    <p className="fw-bold fs-5 pb-3">
+                        Nam sed turpis enim.
+                        Donec semper ante ac eros lobortis gravida.
+                        Aliquam porttitor ligula vel tincidunt commodo.
+                        Phasellus a congue magna, non molestie libero.
+                        Suspendisse potenti.
+                        Ut egestas elit vitae imperdiet tempus.
+                        Maecenas eget felis id est semper pulvinar.
+                        Nunc ac blandit sem.
+                        Nulla imperdiet blandit purus, at semper tortor lacinia eu.
+                        Maecenas ipsum leo, euismod in arcu quis, ultrices consequat urna.
+                        Maecenas lobortis ante et diam euismod, id semper mi malesuada.
+                        In bibendum, quam vel lobortis dictum, enim risus dictum lorem, et blandit augue risus a nisl.
+                        Nunc sem lorem, pellentesque vitae ex at, tempor elementum ex. Etiam maximus aliquam ornare.
+                    </p>
+                </div>
+            </div>
+
+            <div className="wd-add-height-to-scroll"></div>
+        </>
+    );
+
+}
+
+
+export default PrivacyScreen;
