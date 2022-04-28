57611faa8dc6d11b78d2410fcd38fe72f69a29aa
ChokchaiThanrukprasert <chokchaithan@hotmail.com>
2022-04-27 23:27:24 -0400

    Connect Bookmark button with backend server

--- web-dev/src/components/DetailScreen/header-section.js
+++ web-dev/src/components/DetailScreen/header-section.js
@@ -1,37 +1,61 @@
 import React, {useEffect, useState} from "react";
-import {isMovieBookmarkedbyUser} from "../../services/bookmarks-service";
+import {isMovieBookmarkedbyUser, userTogglesBookmark} from "../../services/bookmarks-service";
 import {useParams} from "react-router-dom";
+import {useAuth} from "../../contexts/auth-context";
 
+/**
+ * Detail screen header section.
+ *
+ * It shows movie title (API), poster (API), and bookmark button and status (DB)
+ *
+ * @param movie
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const HeaderSection = ({movie = null}) => {
 
-  // TODO: retrieve bookmark initial state from API
   const {movieId} = useParams()
   const [bookmarked, setBookmark] = useState(false);
   const [isBookmarkDisabled, setBookmarkDisable] = useState(true);
+  const {getUserData} = useAuth();
+  const currentUser = getUserData();
 
-  // Get bookmark status
+  // Get bookmark status once page is loaded
   useEffect(() => {
     setBookmarkDisable(true);
     isMovieBookmarkedbyUser(movieId)
-      .then(response => setBookmark(response.isBookmarked))
+      .then(response => void setBookmark(response.isBookmarked))
       .catch(() => void setBookmark(false))
       .finally(() => void setBookmarkDisable(false));
   }, [movieId]);
+
+  // Toggle bookmark/unbookmark
+  const toggleBookmark = () => {
+    setBookmarkDisable(true);
+    userTogglesBookmark(currentUser.userID, movieId)
+      .then(response => void setBookmark(response.isBookmarked))
+      .finally(() => void setBookmarkDisable(false));
+  }
 
   return (
     <div className="row flex-column-reverse flex-lg-row mb-4">
       <div className="col-12 col-lg-6">
         <h2>
+          {/* Movie title */}
           {movie.title}
+
+          {/* Bookmark button & status */}
           <input type="checkbox" className="btn-check" id="bookmark" autoComplete="off"
                  checked={bookmarked} disabled={isBookmarkDisabled}
-                 onChange={event => setBookmark(event.target.checked)}/>
+                 onClick={toggleBookmark}/>
           <label className="btn btn-outline-warning fw-bold ms-3" htmlFor="bookmark">
             Bookmark <i className="far fa-bookmark"></i>
           </label>
         </h2>
         {movie.plot && <div>{movie.plot}</div>}
       </div>
+
+      {/* Poster */}
       <div className="col-12 col-lg-6 d-flex justify-content-center">
         <img src={movie.image} className="card-img-top detail-poster" alt={movie.title} />
       </div>
