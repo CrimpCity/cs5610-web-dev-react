5ac8329b1a3f3ba9530ef4dbba65f9c9721c02d8
ChokchaiThanrukprasert <chokchaithan@hotmail.com>
2022-04-28 02:38:10 -0400

    Fix avatar image not display on new comments

--- web-dev/src/components/DetailScreen/comment-section.js
+++ web-dev/src/components/DetailScreen/comment-section.js
@@ -5,23 +5,31 @@ import {useAuth} from "../../contexts/auth-context";
 import {createNewCommentForTitle, getCommentsForTitle} from "../../services/comment-service";
 import {useParams} from "react-router-dom";
 
+// Create a provider for comment reducer.
 const CommentsContext = React.createContext(null);
 const initialComments = {comments: [], count: 0};
 
+/**
+ * Reducer for Detail Screen's comment section
+ *
+ * @param state array of displayed comments and count
+ * @param action
+ * @returns {{comments: *[], count: *}|*|{comments, count}|(*&{comments: *})|{comments: *, count: number}|{comments: *[], count: number}}
+ */
 const commentsReducer = (state, action) => {
   switch (action.type) {
     case 'update':
       const updatedComments = state.comments.map(comment => action._id !== comment._id ? {
         ...comment,
         comment: action.comment
-      }: comment);
+      } : comment);
       return {...state, comments: updatedComments};
     case 'delete':
       const deletedComments = state.comments.filter(comment => comment._id !== action._id);
       return {comments: deletedComments, count: state.count - 1};
     case 'create':
       const createdComments = [action.comment, ...state.comments];
-      return {comments:createdComments, count: state.count + 1};
+      return {comments: createdComments, count: state.count + 1};
     case 'get':
       return {comments: action.comments, count: action.count};
     case 'error':
@@ -31,6 +39,15 @@ const commentsReducer = (state, action) => {
   }
 }
 
+/**
+ * Detail Screen's comment section
+ *
+ * Shows comment create box (for critic) and display comments for the displayed
+ * movie.
+ *
+ * @returns {JSX.Element}
+ * @constructor
+ */
 const CommentSection = () => {
   const {getUserData} = useAuth();
   const {movieId} = useParams()
@@ -38,8 +55,18 @@ const CommentSection = () => {
   const [isSendingComment, setSendingComment] = useState(false);
   const [commentTextArea, setCommentTextArea] = useState("");
   const [comments, dispatchComments] = useReducer(commentsReducer, initialComments, (arg) => arg);
+
+  // Handle when user submit a comment.
   const onCreateComment = event => {
     if (!event.shiftKey && event.key === 'Enter') {
+      event.preventDefault();
+
+      // Remind user to enter some comment before creating a comment.
+      if (commentTextArea.trim() === '') {
+        alert("Please enter a comment.");
+        return;
+      }
+
       setSendingComment(true);
       createNewCommentForTitle(movieId, commentTextArea)
         .then(response => {
@@ -47,16 +74,19 @@ const CommentSection = () => {
             _id: response.data._id,
             timestamp: response.data.timestamp,
             comment: commentTextArea,
-            username: currentUser.username
+            username: currentUser.username,
+            avatar: currentUser.avatarImage,
+            userId: currentUser.userID
           };
           dispatchComments({type: "create", comment: newComment});
+          setCommentTextArea('');
         })
         .catch(() => alert('Fail to create a comment. Try again!'))
         .finally(() => void setSendingComment(false));
-      event.preventDefault();
     }
   }
 
+  // Retrieve a list of comments when comment section loaded.
   useEffect(() => {
     getCommentsForTitle(movieId)
       .then(response => {
@@ -66,8 +96,6 @@ const CommentSection = () => {
         dispatchComments({type: "error"});
       })
   }, [movieId])
-
-
 
   return (
     <>
@@ -79,19 +107,22 @@ const CommentSection = () => {
           <div className="list-group-item p-3">
             <div className="row">
               <div className="col-auto">
+                {/* Avatar of the current user (critic) */}
                 <div className="d-flex justify-content-center align-items-center comment-avatar-icon bg-dark">
-                  { currentUser.avatarImage && <img src={currentUser.avatarImage} className="comment-avatar-icon" /> }
-                  { !currentUser.avatarImage && <i className="fas fa-user fa-2x"></i> }
+                  {currentUser.avatarImage && <img src={currentUser.avatarImage} className="comment-avatar-icon" />}
+                  {!currentUser.avatarImage && <i className="fas fa-user fa-2x"></i>}
                 </div>
               </div>
               <div className="col">
-            <textarea className="form-control comment-resize-none"
-                      rows={3}
-                      placeholder="Add a comment..."
-                      disabled={isSendingComment}
-                      value={commentTextArea}
-                      onChange={event => void setCommentTextArea(event.target.value)}
-                      onKeyDown={onCreateComment}></textarea>
+
+                {/* New comment box (critic) */}
+                <textarea className="form-control comment-resize-none"
+                          rows={3}
+                          placeholder="Add a comment..."
+                          disabled={isSendingComment}
+                          value={commentTextArea}
+                          onChange={event => void setCommentTextArea(event.target.value)}
+                          onKeyDown={onCreateComment}></textarea>
               </div>
             </div>
           </div>
@@ -108,5 +139,6 @@ const CommentSection = () => {
     </>
   );
 }
+
 export const useComments = () => useContext(CommentsContext);
 export default CommentSection;
